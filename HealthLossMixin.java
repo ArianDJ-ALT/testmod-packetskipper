31c2e3a71a932598c8c0241c562debd01555ec17
ArianDJ-ALT <galaxygrands@outlook.com>
2024-07-26 15:20:45 +0200

    Create HealthLossMixin.java

---
+++ HealthLossMixin.java
@@ -0,0 +1,18 @@
+package net.yourname.packetblocker.mixin;
+
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class HealthLossMixin {
+
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    public void preventHealthLoss(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        // Blokkeert gezondheidsschade
+        cir.setReturnValue(false);
+    }
+}

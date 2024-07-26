bbb0fb5a5d03e39b6223b58194025d19203dd347
ArianDJ-ALT <galaxygrands@outlook.com>
2024-07-26 15:17:31 +0200

    Create PacketBlockerMod.java

---
+++ PacketBlockerMod.java
@@ -0,0 +1,12 @@
+package net.yourname.packetblocker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PacketBlockerMod implements ModInitializer {
+    public static final String MOD_ID = "packetblocker";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Packet Blocker Mod Initialized");
+    }
+}
